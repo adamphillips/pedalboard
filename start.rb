3d38839a1ed7ecff4ef05e8b96ed4441fb0cf82f
Adam Phillips <aphillips@scholastic.co.uk>
2014-05-05 00:44:25 +0100

    Updated start script to use LED component
    Switched around pin numbers to match current config

--- start.rb
+++ start.rb
@@ -5,36 +5,43 @@ require 'dino'
 require 'micromidi'
 require 'pedalboard'
 
+led1 = nil
+led2 = nil
+led3 = nil
+led4 = nil
+
 pedalboard = Pedalboard.create do
   pot pin: 'A0',
     change: ->(value) { set_volume(value) }
 
-  pedal pin: 5,
+  pedal pin: 2,
     press: ->{ next_set }
 
-  pedal pin: 6,
+  pedal pin: 3,
     press: -> { previous_set }
 
-  pedal pin: 7,
+  pedal pin: 4,
     press: ->{ next_patch }
 
-  pedal pin: 8,
+  pedal pin: 5,
     press: -> { previous_patch }
 
+  led1 = led pin: 13
+  led2 = led pin: 12
+  led3 = led pin: 11
+  led4 = led pin: 10
 end
-
-led = Dino::Components::Led.new(
-  pin: 13, board: pedalboard.board
-)
 
 MIDI.using(pedalboard.midi_input, pedalboard.midi_output) do
   receive do |message|
     if message.index == 22
-      puts message.value.inspect
-      if message.value == 1
-        led.send :on
-      else
-        led.send :off
+      begin
+        led1.light_if ->{ message.value == 1 }
+        led2.light_if ->{ message.value == 2 }
+        led3.light_if ->{ message.value == 3 }
+        led4.light_if ->{ message.value == 4 }
+      rescue Exception => e
+        puts e.message
       end
     end
   end
