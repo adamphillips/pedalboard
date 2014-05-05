e62691ee4b2d1dc4b9903ebf723dcde0323191ea
Adam Phillips <aphillips@scholastic.co.uk>
2014-05-05 22:19:34 +0100

    Added spec for pot

--- lib/pedalboard/components/pot.rb
+++ lib/pedalboard/components/pot.rb
@@ -1,6 +1,8 @@
 require 'dino/components/base_component'
 require 'dino/components/sensor'
 
+require 'pedalboard/components/base_component'
+
 class Pedalboard; module Components
   class Pot < BaseComponent
     TOLERANCE = 2
@@ -8,13 +10,21 @@ class Pedalboard; module Components
     UPPER_LIMIT = 127
     LOWER_LIMIT = 0
 
-    attr_reader :change, :value
+    attr_reader :change, :inverted,
+                :lower_limit, :upper_limit,
+                :tolerance
+
+    attr_accessor :value
 
     def initialize opts={}
       super opts
 
       @change = opts.fetch(:change) { ->(value) {} }
-      @value = 0
+      @lower_limit = opts.fetch(:lower_limit) { LOWER_LIMIT }
+      @upper_limit = opts.fetch(:upper_limit) { UPPER_LIMIT }
+      @tolerance = opts.fetch(:tolerance) { TOLERANCE }
+      @inverted = opts.fetch(:inverted) { false }
+      @value = lower_limit
 
       configure_dino_component
     end
@@ -41,20 +51,25 @@ class Pedalboard; module Components
       )
     end
 
-    def normalised value
-      ((value.to_i / 1024.0) * 127).to_i
+    def normalised received_value
+      normalised_value = (
+        (received_value.to_i / 1024.0) *
+        (upper_limit - lower_limit)
+      ).to_i + lower_limit
+      normalised_value = upper_limit - normalised_value if inverted
+      normalised_value
     end
 
     def range_to_ignore
-      min = value - TOLERANCE
-      max = value + TOLERANCE
+      min = value - tolerance
+      max = value + tolerance
       (min..max).to_a
     end
 
     def significant_change? new_value
       if new_value != value
-        return true if new_value == LOWER_LIMIT
-        return true if new_value == UPPER_LIMIT
+        return true if new_value == lower_limit
+        return true if new_value == upper_limit
       end
 
       !range_to_ignore.include?(new_value)
---
+++ spec/unit/pedalboard/components/pot_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+require 'pedalboard/components/pot'
+
+class Pedalboard; module Components
+  describe Pot do
+    def dino_component
+      @dino_component ||= Dino::Components::Sensor.new(board: board, pin: 1)
+    end
+
+    let(:tolerance) { 10 }
+    let(:upper_limit) { 110 }
+    let(:lower_limit) { 10 }
+    let(:inverted) { false }
+    let(:change) { ->(value) { puts 'poop'; change_value = value } }
+    #let(:dino_component) { double }
+    let(:board) { double(add_analog_hardware: nil, start_read: nil) }
+    let(:pedalboard) { double(board: board) }
+
+    subject { Pot.new(
+      tolerance: tolerance,
+      upper_limit: upper_limit,
+      lower_limit: lower_limit,
+      inverted: inverted,
+      change: change,
+      pedalboard: pedalboard,
+      dino_component: dino_component
+    ) }
+
+    context 'when the dino component recieves data' do
+      it 'should call the change proc with the normalised value' do
+        expect(subject.value).to equal(10)
+        expect(subject).to receive(:run_command).with(change, 35)
+        dino_component.update 256
+        expect(subject.value).to equal(35)
+      end
+
+      context 'when the change is within the tolerance' do
+        it 'should not change the value' do
+          subject.value = 34
+          expect(subject).to_not receive(:run_command).with(change, 35)
+          dino_component.update 256
+          expect(subject.value).to equal(34)
+        end
+      end
+
+      context 'when inverted' do
+        let(:inverted) { true }
+
+        it 'should call the change proc with the inverted value' do
+          expect(subject).to receive(:run_command).with(change, 75)
+          dino_component.update 256
+          expect(subject.value).to equal(75)
+        end
+      end
+    end
+  end
+end; end
